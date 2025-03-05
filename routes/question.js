bc0e61727247209c7074d7399c5eb26a1486ca16
Jad Radwan <jad.radwan@outlook.com>
2025-03-05 21:24:07 +0200

    fixed

--- routes/question.js
+++ routes/question.js
@@ -8,6 +8,7 @@ router.get('/',(req,res)=>
     res.sendFile(path.join(__dirname,'..','Frontend','HTML','Q&ASection.html'));
 })
 router.get('/latest', qnaController.getLatestQuestions);
+router.post('/reply',qnaController.handleReply);
 
 router.post('/',qnaController.postQuestion);
 module.exports = router;
