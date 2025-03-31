bf76343a5438f5e8fab272ea9e039b963bb727b3
AliZreik05 <azreik005@gmail.com>
2025-03-31 20:57:41 +0300

    fixed the following: 1.When a user is not logged in he shouldn’t be able to go to internship page using the explore internship button 2.The internship should by placed under each other and not next to each other

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ app.use('/forgot',require('./routes/forgot'))
 app.use('/logout', require('./routes/logout'));
 app.use('/admin/login', require('./routes/adminLogin'));
 app.use('/admin', verifyJWT, checkAdmin, require('./routes/admin'));
-app.use('/internships',require('./routes/internship'));
 app.use(verifyJWT,updateLastActivity);
+app.use('/internships',require('./routes/internship'));
 app.use('/profile',require('./routes/profile'));
 app.use('/reviews',require('./routes/reviews'));
 app.use('/Q&A',require('./routes/question'));
