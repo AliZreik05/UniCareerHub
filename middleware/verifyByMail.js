1ee19b497ffaa4e1c8b21980bce449228ed27d89
AliZreik05 <azreik005@gmail.com>
2025-03-31 23:11:10 +0300

    added real verification by mail service

--- middleware/verifyByMail.js
+++ middleware/verifyByMail.js
@@ -1,28 +1,44 @@
-const nodemailer = require('nodemailer');
+const {google} = require ('googleapis')
+const nodemailer = require('nodemailer')
+require('dotenv').config();
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID,CLIENT_SECRET,REDIRECT_URI)
+oAuth2Client.setCredentials({refresh_token:REFRESH_TOKEN})
 
 async function sendVerificationEmail(userEmail,generatedCode)
 {
-    const transporter = nodemailer.createTransport(
-        {
-            host: 'smtp.ethereal.email',
-            port: 587,
-            auth: {
-              user: 'lelah.sanford98@ethereal.email',
-              pass: 'hXbqqrw5yA9v6TCpnf'
+    try{
+        const accessToken = await oAuth2Client.getAccessToken()
+        const transport = nodemailer.createTransport({
+            service:'gmail',
+            auth:
+            {
+                type:'OAuth2',
+                user: 'unicareerhub271@gmail.com',
+                clientId : CLIENT_ID,
+                clientSecret : CLIENT_SECRET,
+                refreshToken : REFRESH_TOKEN,
+                accessToken: accessToken
             }
-        }
-    );
-    let info = await transporter.sendMail({
-        from: '"UniCareerHub" <no-reply@UniCareerHub.com>',
-        to: userEmail,
-        subject: 'Verify Your Email Address',
-        text: `Your verification code is: ${generatedCode}`,
-        html: `<p>Your verification code is: <strong>${generatedCode}</strong></p>`
-      });
-      
-    console.log('Message sent: %s', info.messageId);
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-}
+        })
 
+        const mailOptions =
+        {
+            from: '"UniCareerHub" <no-reply@UniCareerHub.com>',
+            to: userEmail,
+            subject: 'Verify Your Email Address',
+            text: `Your verification code is: ${generatedCode}`,
+            html: `<p>Your verification code is: <strong>${generatedCode}</strong></p>`
+        }
+        const result = await transport.sendMail(mailOptions)
+        return result
+    }catch (error)
+    {
+        error
+    }
+}
 module.exports = sendVerificationEmail;
